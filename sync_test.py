16f97bb304d4c07c339429869257b581878c8557
tpdns90321 <tpdns9032100@gmail.com>
2019-05-23 17:18:39 +0900

    [수정] 갯수 변경 삭제

--- sync_test.py
+++ sync_test.py
@@ -1,9 +1,11 @@
+import gevent
+
 from sync import Sync
 from settings import generateDefaultSetting
 from BoardCode import HAKSA
 
 if __name__ == "__main__":
    setting = generateDefaultSetting()
-   setting["rw-workers"] = 30
    sync = Sync(setting, HAKSA)
    sync.firstRun()
+
