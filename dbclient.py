0f7ff839703155075debc6ad64f3ef8bc53fb42f
tpdns90321 <tpdns9032100@gmail.com>
2019-05-23 16:41:19 +0900

    [주석] 오버라이드한 함수의 설명을 추가한다.(DBClient)

--- dbclient.py
+++ dbclient.py
@@ -10,8 +10,11 @@ class DBClient(Redis):
                        port=Settings["redis-port"],
                        password=Settings["redis-password"],
                        db=BoardCode["dbid"])
+
+    # key 값은 그대로 놓고 value만 json으로 변환해서 저장한다.
     def set(self, key, value):
         Redis.set(self, key, json.dumps(value, ensure_ascii=False))
 
+    # key 값은 그대로 놓고 결과값만 json에서 python 구조체로 변환해서 볼러온다.
     def get(self, key):
         return json.loads(Redis.get(self, key).decode("utf-8"))
