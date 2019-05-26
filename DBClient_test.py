b576ce8e33a64bc4c850a0b566c17bc2a4f3c9f3
tpdns90321 <tpdns9032100@gmail.com>
2019-05-26 10:11:42 +0900

    [TEST] 키 누락 확인 테스트

--- DBClient_test.py
+++ DBClient_test.py
@@ -9,8 +9,11 @@ def dbLogin():
 if __name__ == "__main__":
     db = dbLogin()
     keys = db.keys(True)
+    # sync_test.py 를 작동 후 실행을 가정
     print(keys[0], keys[-1])
     print(db.get(1))
     print(db.get(keys[-1], False))
     print(keys[len(keys)-3:len(keys)])
+    for i in range(len(keys)):
+        assert i+1 == keys[i]
 
