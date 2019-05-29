d1915b22efb7c31d8070913af281ec808b5277d6
tpdns90321 <tpdns9032100@gmail.com>
2019-05-29 18:49:42 +0900

    [추가] api 클래스를 추가하고 flask 의존성 추가

---
+++ api.py
@@ -0,0 +1,19 @@
+import json
+
+from DBClient import DBClient
+
+class API(DBClient):
+    def __INIT__(self, Setting, BoardCode):
+        DBClient.__INIT__(self, Setting, BoardCode)
+
+    def get(self, page, perView):
+        keys = self.keys(sort=True)
+        start = len(keys) - page * perView
+        end = len(keys) - (page - 1) * perView
+        start = 0 if start < 0 else start
+        end = 0 if end < 0 else end
+        query = []
+        for k in keys[start:end]:
+            query.append(DBClient.get(self,k))
+        query.reverse()
+        return json.dumps(query, ensure_ascii=False)
