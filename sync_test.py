93368931cbc4cf87836791d4d330223dd28c1a34
tpdns90321 <tpdns9032100@gmail.com>
2019-05-29 16:45:10 +0900

    [TEST] sync_test의 테스트 세분화

--- sync_test.py
+++ sync_test.py
@@ -8,17 +8,19 @@ from BoardCode import HAKSA
 if __name__ == "__main__":
     setting = generateDefaultSetting()
     sync = Sync(setting, HAKSA)
-    if len(sys.argv) > 1 and sys.argv[1] == "firstRun":
-        sync.firstRun()
-        print(len(sync.keys()))
+    if len(sys.argv) > 1: 
+        if sys.argv[1] == "firstRun":
+            sync.firstRun()
+            print(len(sync.keys()))
 
-    keys = len(sync.keys())
-    [sync.delete(i) for i in range(keys-3,keys+1)]
-    print(len(sync.keys()))
-    sync.Run()
-    print(len(sync.keys()))
-    [sync.delete(i) for i in range(keys-30,keys+1)]
-    print(len(sync.keys()))
-    sync.Run()
-    print(len(sync.keys()))
+        elif sys.argv[1] == "Run":
+            keys = len(sync.keys())
+            [sync.delete(i) for i in range(keys-3,keys+1)]
+            print(len(sync.keys()))
+            sync.Run()
+            print(len(sync.keys()))
+            [sync.delete(i) for i in range(keys-30,keys+1)]
+            print(len(sync.keys()))
+            sync.Run()
+            print(len(sync.keys()))
 
